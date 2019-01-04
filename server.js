e0f3ecfd1b24f2c61219c1688aceafc2c5f4430d
Ubuntu <ubuntu@ip-172-31-20-236.us-east-2.compute.internal>
2019-01-04 20:33:03 +0000

    test locally

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app.js");
 const cors = require("cors");
 
 // app.use(cors());
-
+console.log(process.env.PRODUCTION_URL);
 const port = process.env.PORT || 9001;
 
 app.listen(port, () => {
