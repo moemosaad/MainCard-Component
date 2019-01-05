bdc67a0ae2f1221bb4da6866f3150b5f04ac12af
Nick <nbisigna@gmail.com>
2019-01-05 09:18:08 -0500

    css

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 // app.use(cors());
 
 const port = process.env.PORT || 9001;
-console.log(process.env);
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
