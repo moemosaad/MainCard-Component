2051634c12d894ea200c0b1e10839edb9abbee01
Nick <nbisigna@gmail.com>
2019-01-05 10:43:40 -0500

    spaghetti

--- app.js
+++ app.js
@@ -2,20 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
-const compression = require('compression');
+const cors = require("cors");
+const compression = require("compression");
 const app = express();
 
 const db = require("./db/config.js");
 
 //uncomment to seed data
 // const seed = require("./db/seed.js");
-
-app.use(bodyParser.json());
-app.use(express.static(__dirname + "/public"));
-app.use(compression());
 app.use(cors());
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -24,6 +19,10 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+app.use(compression());
 
 const routes = require("./routes/routes.js");
 
