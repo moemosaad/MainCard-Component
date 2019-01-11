c73ede98f29e928636f65d0f3d33c7f2503004e9
Muhammad Mosaad <mm458@njit.edu>
2019-01-10 22:10:19 -0500

    batch save to csv and mongo db

--- db/generateData.js
+++ db/generateData.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const Movie = require("../models/Movie.js");
 const db = require("./config.js");
-const converter = require("json-2-csv");
+// const seedMongoData = require("./seed.js");
+console.time("dbsave");
 let data = JSON.parse(fs.readFileSync(__dirname + "/data.json").toString());
+// console.log(data);
 let dataInfo = {
   title: "video",
   year: "video",
@@ -19,7 +21,6 @@ let dataInfo = {
 };
 
 let randomData = {};
-let newData = [];
 
 var extractData = (infoObj, info) => {
   if (Array.isArray(infoObj)) {
@@ -28,71 +29,123 @@ var extractData = (infoObj, info) => {
   return data.map(item => item[infoObj][info]);
 };
 
-var extractRandomData = () => {
+var collectRandomData = () => {
   for (var key in dataInfo) {
     randomData[key] = extractData(dataInfo[key], key);
   }
 };
-
-var generateRandomData = () => {
-  for (var i = 101; i < 1000101; i++) {
-    newData[i] = {
-      id: i,
-      video: {
-        title: randomize("title"),
-        year: randomize("year"),
-        video: randomize("video")
-      },
-      poster: {
-        image: randomize("image")
-      },
-      score: {
-        all_critics: {
-          tomatometer: randomize("tomatometer"),
-          average_rating: randomize("average_rating"),
-          reviews_counted: randomize("reviews_counted"),
-          fresh: randomize("fresh")
-          // get rotten() {
-          //   return this.reviews_counted - this.fresh;
-          // }
-        },
-        consensus: randomize("consensus"),
-        audience: {
-          audience_score: randomize("audience_score"),
-          average_rating: randomize("average_rating"),
-          user_ratings: randomize("user_ratings")
-        },
-        top_critics: {
-          tomatometer: randomize("tomatometer"),
-          average_rating: randomize("average_rating"),
-          reviews_counted: randomize("reviews_counted"),
-          fresh: randomize("fresh")
-          // get rotten() {
-          //   return this.reviews_counted - this.fresh;
-          // }
-        }
-      }
-    };
-    newData[i].score.all_critics.rotten =
-      newData[i].score.top_critics.reviews_counted -
-      newData[i].score.top_critics.fresh;
-    newData[i].score.top_critics.rotten =
-      newData[i].score.top_critics.reviews_counted -
-      newData[i].score.top_critics.fresh;
-    // console.log(newData);
-  }
-  if (i % 100 === 0) {
-    console.log(i, "was inserted");
-  }
-};
-
 var randomize = data => {
   let dataArr = randomData[data];
   return dataArr[Math.floor(Math.random() * Math.floor(dataArr.length))];
 };
 
-var json2csvCallback;
+var decorateNewData = (newData, index) => {
+  newData = {
+    id: index,
+    video: {
+      title: randomize("title"),
+      year: randomize("year"),
+      video: randomize("video")
+    },
+    poster: {
+      image: randomize("image")
+    },
+    score: {
+      all_critics: {
+        tomatometer: randomize("tomatometer"),
+        average_rating: randomize("average_rating"),
+        reviews_counted: randomize("reviews_counted"),
+        fresh: randomize("fresh")
+      },
+      consensus: randomize("consensus"),
+      audience: {
+        audience_score: randomize("audience_score"),
+        average_rating: randomize("average_rating"),
+        user_ratings: randomize("user_ratings")
+      },
+      top_critics: {
+        tomatometer: randomize("tomatometer"),
+        average_rating: randomize("average_rating"),
+        reviews_counted: randomize("reviews_counted"),
+        fresh: randomize("fresh")
+      }
+    }
+  };
+  newData.score.all_critics.rotten =
+    newData.score.top_critics.reviews_counted - newData.score.top_critics.fresh;
+  newData.score.top_critics.rotten =
+    newData.score.top_critics.reviews_counted - newData.score.top_critics.fresh;
+  return newData;
+};
 
-extractRandomData();
+var generateRandomData = () => {
+  collectRandomData();
+  fs.writeFile("./newData.csv", "", () => {
+    var newData;
+    var dataArr;
+    var batchSize = 800;
+    var limit = 20000;
+    // var total = 0;
+    var start = 0;
+    var end = batchSize;
+    var batchData = "";
+    var csvfile = fs.createWriteStream("./newData.csv");
+    var storeBatch = () => {
+      // dataArr = [];
+      // for (var i = start + 101; i < end + 101; i++) {
+      //   let index = i - 101;
+      //   var decoratedData = decorateNewData(newData, i);
+      //   dataArr.push(decoratedData);
+      // }
+      // Movie.collection.insertMany(dataArr, () => {
+      //   if (total < limit - batchSize) {
+      //     total += batchSize;
+      //     console.log(start, end);
+      //     storeBatch((start += batchSize), (end += batchSize));
+      //   } else {
+      //     console.log(start, end);
+      //     console.timeEnd("dbsave");
+      //     return;
+      //   }
+      // });
+      var writeToFile = (newData, file, num, end) => {
+        if (num === 101) {
+          // file.write("[" + JSON.stringify(newData) + ", ");
+          batchData += "[" + JSON.stringify(newData) + ", ";
+          console.log("ok");
+        } else if (num >= end) {
+          console.log("ok");
+          // file.write(JSON.stringify(newData) + "]");
+          batchData += JSON.stringify(newData) + "]";
+          file.write(batchData);
+          console.timeEnd("dbsave");
+          console.log(end);
+          return;
+        } else if (num % batchSize === 101) {
+          // file.write(JSON.stringify(newData) + ", ");
+          batchData += JSON.stringify(newData) + ", ";
+          file.write(batchData);
+          batchData = "";
+        } else {
+          batchData += JSON.stringify(newData) + ", ";
+        }
+      };
+      for (var i = 101; i < limit + 102; i++) {
+        // let index = i - 101;
+        var decoratedData = decorateNewData(newData, i);
+        writeToFile(decoratedData, csvfile, i, limit + 101);
+        // console.log(batchData);
+        // console.log(i);
+      }
+    };
+    storeBatch();
+    csvfile.end(() => {
+      console.log("end");
+      return;
+    });
+  });
+};
+
 generateRandomData();
-module.exports = newData;
+
+// module.exports = generateRandomData();
--- db/seed.js
+++ db/seed.js
@@ -1,50 +1,63 @@
 const fs = require("fs");
-const v8 = require("v8");
-const cluster = require("cluster");
+// const v8 = require("v8");
+// const cluster = require("cluster");
 console.time("dbsave");
+const split = require("split");
 const Movie = require("../models/Movie.js");
-const newData = require("./generateData.js");
+// const newData = require("./generateData.js");
 const db = require("./config.js");
-v8.setFlagsFromString("--max-old-space-size=4096");
+// v8.setFlagsFromString("--max-old-space-size=4096");
 // const dbPostGres = require("./seedPostgres.js");
 
-// let data = fs.readFileSync(__dirname + "/data.json");
+let data =
+  // JSON.parse(
+  fs
+    .createReadStream("./newData.csv")
+    .pipe(split(JSON.parse))
+    .on("data", batch => {
+      // Movie.collection.insertMany(batch, () => {});
+      console.log(batch);
+    });
+// );
+// JSON.parse(fs.readFileSync(__dirname + "/newData.csv", "utf-8"));
+console.log(data);
+console.log(data.length);
 
-console.log(newData.length);
-const seedDataMongo = data => {
-  let numCores = require("os").cpus().length;
-  let start = 0;
-  let batchSize = 800;
-  let end = batchSize;
+// const seedDataMongo = data => {
+// let numCores = require("os").cpus().length;
+const seedDataMongo = (start, end, batchSize, data) => {
+  // let start = 0;
+  // let batchSize = 800;
+  // let end = batchSize;
 
-  console.log(numCores);
-  var setUpWorkerProcesses = () => {
-    if (cluster.isMaster) {
-      for (let i = 0; i < numCores; i++) {
-        cluster.fork();
-      }
-      cluster.on("exit", function(worker, code, signal) {
-        console.log(`worker ${worker.process.pid} died`);
-      });
-    }
-  };
+  // var setUpWorkerProcesses = () => {
+  //   if (cluster.isMaster) {
+  //     for (let i = 0; i < numCores; i++) {
+  //       cluster.fork();
+  //     }
+  //     cluster.on("exit", function(worker, code, signal) {
+  //       console.log(`worker ${worker.process.pid} died`);
+  //     });
+  //   }
+  // };
 
   var storeBatch = () => {
     console.log(start, end, data.length);
     var dataBatch = data.slice(start, end);
+    console.log(dataBatch.length);
     Movie.collection.insertMany(dataBatch, () => {
       if (start < data.length) {
         storeBatch((start += batchSize), (end += batchSize));
-      } else {
+      } else if (start >= data.length) {
         console.timeEnd("dbsave");
       }
     });
   };
   // setUpWorkerProcesses();
-  storeBatch(start, end);
+  // storeBatch(start, end);
 };
 
-seedDataMongo(newData);
+// seedDataMongo(data);
 // for (var start = 0; start < data.length; start += 800) {
 //   var end = start + 800;
 //   console.log(start, end);
@@ -57,56 +70,5 @@ seedDataMongo(newData);
 //   console.log(err);
 // });
 // }
-
-// Promise.all(
-//   data.map(movie => {
-//     return Movie.findOneAndUpdate(
-//       { id: movie.id },
-//       {
-//         id: movie.id,
-//         video: {
-//           title: movie.video.title,
-//           year: movie.video.year,
-//           video: movie.video.video
-//         },
-//         poster: {
-//           image: movie.poster.image
-//         },
-//         score: {
-//           all_critics: {
-//             tomatometer: movie.score.all_critics.tomatometer,
-//             average_rating: movie.score.all_critics.average_rating,
-//             reviews_counted: movie.score.all_critics.reviews_counted,
-//             fresh: movie.score.all_critics.fresh,
-//             rotten: movie.score.all_critics.rotten
-//           },
-//           consensus: movie.score.consensus,
-//           audience: {
-//             audience_score: movie.score.audience.audience_score,
-//             average_rating: movie.score.audience.average_rating,
-//             user_ratings: movie.score.audience.user_ratings
-//           },
-//           top_critics: {
-//             tomatometer: movie.score.top_critics.tomatometer,
-//             average_rating: movie.score.top_critics.average_rating,
-//             reviews_counted: movie.score.top_critics.reviews_counted,
-//             fresh: movie.score.top_critics.fresh,
-//             rotten: movie.score.top_critics.rotten
-//           }
-//         }
-//       },
-//       {
-//         upsert: true
-//       }
-//     ).exec();
-//   })
-// )
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   })
-// };
-
+module.exports = seedDataMongo;
 // module.exports = seedData(newData);
