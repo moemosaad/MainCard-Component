21c4dc744907c147a93f129e476e672cb3cc16df
Muhammad Mosaad <mm458@njit.edu>
2019-01-23 16:13:13 -0500

    added load balancer

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,11 +1,11 @@
-const mongo = require("../../db/config.js");
+// const mongo = require("../../db/config.js");
 const postgres = require("../../db/PostgreSQL/connectPostgres.js");
 const helper = require("./helper/routeHelpers.js");
 
 module.exports = {
   getOne: (req, res) => {
     let id = parseInt(req.params.id);
-    console.time("Time to query Postgres");
+    // console.time("Time to query Postgres");
     // console.time("Time to query Mongo");
     // mongo.then(client => {
     //   client
@@ -21,14 +21,14 @@ module.exports = {
     //       res.send(500);
     //     });
     // });
+
     postgres.query(`SELECT * FROM movies m WHERE m.m = ${id}`, (err, data) => {
       if (err || Array.isArray(data)) {
-        console.log("Postgres query error: ", err);
+        // console.log("Postgres query error: ", err);
         res.send(500);
       } else {
-        console.timeEnd("Time to query Postgres");
+        // console.timeEnd("Time to query Postgres");
         data = helper.mapData(data.rows[0]);
-        console.log(data);
         res.send(data);
       }
     });
